2d084ec627c56cc2335137e27351ae35f87a9f27
BAYRON PICADO OBANDO <bayron.picado.obando@est.una.ac.cr>
2020-07-12 11:05:51 -0600

    End of section 3

--- API/Controllers/ValuesController.cs
+++ API/Controllers/ValuesController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Domain;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace DatingApp.API.Controllers
 {
+    [EnableCors("CorsPolicy")]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,11 @@ namespace API
             services.AddDbContext<DataContext>(opt => {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => {
+               opt.AddPolicy("CorsPolicy", policy => {
+                  policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+               });
+            });
             services.AddControllers();
         }
 
@@ -45,7 +50,9 @@ namespace API
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            // app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
